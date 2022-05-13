70c614f81be6af6afc9345844fbe6d1f237a2d29
coderlmw <lmwl0113@163.com>
2022-05-13 23:58:50 +0800

    项目初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'normalize.css'
+import './assets/css/index.less'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: './build',
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+  }
 })
